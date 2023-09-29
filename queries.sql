e916bde673edf188b2833168fcf004501b651550
Young-Gaius <youngehu8@gmail.com>
2023-09-29 15:32:37 +0100

    updated animal table

--- queries.sql
+++ queries.sql
@@ -7,3 +7,56 @@ SELECT name, escape_attempts  FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.3 AND weight_kg <= 17.3;
+
+
+
+-- Start the transaction
+BEGIN;
+-- Update the animals table by setting the species column to 'unspecified
+UPDATE animals 
+SET species = 'unspecified'
+
+-- Verify the changes
+SELECT * FROM animals;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+-- Start the transaction for delete
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+-- Verify if all records in the animals table still exist
+SELECT * FROM animals;
+SAVEPOINT my_savepoint;
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SAVEPOINT my_savepoint;
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+-- Write queries to answer the following questions
+
+-- How many animals are there
+SELECT COUNT(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT COUNT( escape_attempts) FROM animals WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, COUNT(*) FROM animals WHERE escape_attempts > 0 GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -11,3 +11,31 @@ CREATE Table animals(
   weight_kg DECIMAL,
   PRIMARY KEY(id)
 );
+
+-- add species column to animals table
+ALTER TABLE animals
+ADD species VARCHAR(100);
+
+-- creating owner table
+CREATE Table owners(
+  id  INTEGER GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(50),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+-- creating species table
+CREATE Table species(
+  id  INTEGER GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50),
+  PRIMARY KEY(id)
+);
+
+-- remove species column
+ALTER TABLE animals
+DROP  COLUMN  species;
+
+--add foreign key to animals table
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id ) REFERENCES species(id);
+ADD FOREIGN KEY (owner_id ) REFERENCES owners(id);
