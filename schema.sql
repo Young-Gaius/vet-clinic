ce2edf710acf63e7e18ac942385afc6804cd66c4
Young-Gaius <youngehu8@gmail.com>
2023-09-29 12:52:15 +0100

    create animal table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
+/* table for animals */
+CREATE Table animals(
+  id  INTEGER GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  PRIMARY KEY(id)
 );
